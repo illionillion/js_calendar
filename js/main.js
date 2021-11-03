9559f7666e3c4dc2352a945ded81b2f63368318f
Yuto Ichinose <yuto.0526.sano@gmail.com>
2021-11-03 18:48:02 +0900

    来月、先月のカレンダーへ移動するボタン

--- js/main.js
+++ js/main.js
@@ -5,23 +5,57 @@ let date_data=[
   ["日","月","火","水","木","金","土"]
 ]
 
+//来月取得
 function next_month() {
-  let now_m=document.getElementById("month_select").value;
+  const ele_y=document.getElementById("year_select");
+  let now_y=ele_y.value;
+  const ele_m=document.getElementById("month_select");
+  let now_m=ele_m.value;
   console.log(Number(now_m)+1);
-  document.getElementById("month_select").querySelector("option[value='"+now_m+"']").setAttribute("selected", "");
   now_m=Number(now_m)+1
-  // document.getElementById("month_select").options[now_m].selected=true;
-  document.getElementById("month_select").querySelector("option[value='"+now_m+"']").setAttribute("selected", "selected");
+  if (now_m>12) {
+    now_m=1;
+    now_y++;
+    if (getOptionElement(ele_y,now_y)) {
+        ele_y.selectedIndex=getOptionElement(ele_y,now_y);
+    }else{
+      return;
+    }
+  }
+  console.log(getOptionElement(ele_m,now_m));
+  ele_m.selectedIndex=getOptionElement(ele_m,now_m);
+  get_day();
+  make_table();
+}
+//先月取得
+function last_month() {
+  const ele_y=document.getElementById("year_select");
+  let now_y=ele_y.value;
+  const ele_m=document.getElementById("month_select");
+  let now_m=ele_m.value;
+  console.log(Number(now_m)-1);
+  now_m=Number(now_m)-1
+  if (now_m<1) {
+    now_m=12;
+    now_y--;
+    if (getOptionElement(ele_y,now_y)) {
+        ele_y.selectedIndex=getOptionElement(ele_y,now_y);
+    }else{
+      return;
+    }
+  }
+  console.log(getOptionElement(ele_m,now_m));
+  ele_m.selectedIndex=getOptionElement(ele_m,now_m);
+  get_day();
+  make_table();
 }
 
-function last_month() {
-  let now_m=document.getElementById("month_select").value;
-  console.log(Number(now_m)-1);
-  // document.getElementById("month_select").options[now_m].selected=false;
-  document.getElementById("month_select").querySelector("option[value='"+now_m+"']").setAttribute("selected", "");
-  now_m=Number(now_m)-1;
-  // document.getElementById("month_select").options[now_m].selected=true;
-  document.getElementById("month_select").querySelector("option[value='"+now_m+"']").setAttribute("selected", "selected");
+//selectタグのvalueからindexを取得するプログラム
+function getOptionElement(select_tag , value){
+  for(var i=0; i<select_tag.options.length; i++){
+    if(select_tag.options[i].value == value){return i;}
+  }
+  return null;
 }
 
 window.onload=function(){
@@ -37,11 +71,11 @@ window.onload=function(){
     get_day();
     make_table();
   })
-  // document.getElementById("last").addEventListener("click",function () {
-  //   last_month();
-  // })
-  // document.getElementById("next").addEventListener("click",function () {
-  //   next_month();
-  // })
+  document.getElementById("last").addEventListener("click",function () {
+    last_month();
+  })
+  document.getElementById("next").addEventListener("click",function () {
+    next_month();
+  })
 
 }
