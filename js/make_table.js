e6ac71c4290b976ee727e4036d8cc5490e912578
Yuto Ichinose <yuto.0526.sano@gmail.com>
2021-11-03 18:47:28 +0900

    カスタムデータ属性で日付や一の情報をつけて取り出せるようにした

--- js/make_table.js
+++ js/make_table.js
@@ -19,11 +19,27 @@ function make_table(){
     for(let j=0;j<date_data[i].length;j++){
 
       let td_ele=document.createElement("td");
-      td_ele.innerHTML=delete_undefined(date_data[i][j]);
+      td_ele.innerHTML=pluszero(date_data[i][j]);
+      // td_ele.setAttribute("data-date",date_data[i][j]);
+      td_ele.dataset.date=date_data[i][j]; //カスタムデータ設定
+      // td_ele.setAttribute("data-row",i);
+      td_ele.dataset.row=i;
+      // td_ele.setAttribute("data-col",j);
+      td_ele.dataset.col=j;
+      td_ele.addEventListener("click",function () {
+        get_customdata(this);
+      })
       tr_ele.appendChild(td_ele);
 
     }
     
   }
 
+}
+
+function get_customdata(ele) { //customデータを取得
+  console.log(ele);
+  console.log(ele.dataset.date);
+  console.log(ele.dataset.row);
+  console.log(ele.dataset.col);
 }
