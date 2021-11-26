0c0a11c3d10e1b06c4430d6f043c028d2a85a5b9
Yuto Ichinose <yuto.0526.sano@gmail.com>
2021-11-27 00:35:31 +0900

    ファイル名を年月になるようにした。

--- js/take_screenshot.js
+++ js/take_screenshot.js
@@ -7,7 +7,7 @@ function take_ss(){
         const imgData = canvas.toDataURL();
         const downloadtag=document.createElement("a");
         // document.body.appendChild(downloadtag); //appendChildする必要あるのか？
-        downloadtag.download="カレンダー";
+        downloadtag.download=$("#year_select").val()+"年"+$("#month_select").val()+"月カレンダー";
         downloadtag.href=imgData;
         downloadtag.click();
         // downloadtag.remove();
