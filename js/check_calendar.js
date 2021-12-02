a2f4c17ead063c654b1ddbf1f89f9e9cd9e25cec
Yuto Ichinose <yuto.0526.sano@gmail.com>
2021-12-02 23:19:42 +0900

    IMEパッドとやらでパイプ打てるようになった||

--- js/check_calendar.js
+++ js/check_calendar.js
@@ -1,5 +1,7 @@
 function check_calendar(ele){
     //||パイプが打てないことが判明した
+    //||
+    // console.log(true || false);
     if ($(ele).data("date")!="　"&&$(ele).data("week")) {
         $(ele).toggleClass("check");
     }
